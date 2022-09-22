5f4736289af57d685922b4ff5618762a15f11eb4
Deepak Sharma <112468661+deep-bits@users.noreply.github.com>
2022-09-22 10:57:27 +0530

    Documentation for R script
    Provides a demonstration on how iteration and programming can be useful for problems like automation

--- Populate_SQL_table.R
+++ Populate_SQL_table.R
@@ -1,4 +1,8 @@
-for (i in 1:500) {
-  cat("\nINSERT INTO BUYERS  ( ID, NAME, DOB )
-  VALUES (", i, ", 'DINESH', '1991-09-13');")
+# Using a for loop to generate a text
+# This is a demonstration of how iteration (loops) can be used to perform repeated operations
+# The following for loop will print SQL's INSERT INTO statement with unique value for `ID` attribute
+
+for (i in 1:5) {
+    cat("\nINSERT INTO BUYERS  ( ID, NAME, DOB ) # As ID shall be unique, we use iteration
+  VALUES (",i,",'DINESH', '1991-09-13');") # You can also randomly generate Name and Date (Not required for now)
 }
